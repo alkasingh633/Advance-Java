5ac0fc72db76398079ea673ac6e800285ba35052
Alka <alkasingh143179@gmail.com>
2026-03-16 18:20:39 +0530

    commit2

---
+++ p1/java/A2.java
@@ -0,0 +1,11 @@
+package p1.java;
+
+public class A2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello new...............");
+
+	}
+
+}

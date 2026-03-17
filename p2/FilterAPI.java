ab11873734e37ee863bd0658b034194daa2c2c29
Alka <alkasingh143179@gmail.com>
2026-03-17 09:45:13 +0530

    commit

--- p2/FilterAPI.java
+++ p2/FilterAPI.java
@@ -1,30 +1,29 @@
 package p2;
-
-	import java.util.ArrayList;
+    import java.util.ArrayList;
 	import java.util.List;
 	import java.util.stream.Stream;
 	import java.util.function.Predicate;
 
 
-	public class A1 {
+	public class FilterAPI {
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
 			List<Integer> list = new ArrayList<Integer>();
-			
 			list.add(23);
 			list.add(45);
 			list.addAll(List.of(23,34,56,78,65));
 			System.out.println(list);
 //			Stream listStream = list.stream();
 			list.stream().filter(n->n>60).forEach((element)-> System.out.println(element));
-			list.stream().forEach((element)->System.out.print(element + " "));
+//			list.stream().forEach((element)->System.out.print(element + " "));
 			list.stream().forEach(System.out::println);
-			System.out.println("count =" +list.stream().filter((iteam)->iteam>60).count());
+//			System.out.println("count =" +list.stream().filter((iteam)->iteam>60).count());
 			
 
-		
+		}
 
 	}
 
-}
+
+

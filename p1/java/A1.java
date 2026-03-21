44c067ec864b58f4d465106cda082809d165359f
Alka <alkasingh143179@gmail.com>
2026-03-21 09:42:59 +0530

    21-03-2026

--- p1/java/A1.java
+++ p1/java/A1.java
@@ -1,11 +1,40 @@
 package p1.java;
 
-public class A1 {
+		class Sum {
+			int num1, num2;
+		    Sum(int num1, int num2){
+		    	this.num1=num1;
+		    	this.num2=num2;
+		    }
+		    void getSum() {
+		    	int s=num1+num2;
+		    	System.out.println("Sum="+s);
+		    }
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello........");
+		class Sub {
+			void getSub(int num1, int num2) {
+				int s=num1-num2;
+		    	System.out.println("Subtraction="+s);
+			}
+		}
+
+		public class A1 {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		           Sum s1 =new Sum(23,45);
+		           s1.getSum();
+		           Sub sub1 =new Sub();
+		           sub1.getSub(34, 56);
+		           
+			}
+
+		
+
+
+
 
 	}
 
-}
+
--- p1/java/A2.java
+++ p1/java/A2.java
@@ -1,11 +1,34 @@
 package p1.java;
 
+
+
+class DisplayMessage {
+	String msg;
+	 
+	 DisplayMessage(String msg){
+		 this.msg=msg;
+	 }
+	 DisplayMessage(){
+		  this("This is Java 8+");
+		  System.out.println("This is constructor");
+	 }
+	 void displayInfo() {
+		 System.out.println(msg);
+	 }
+}
+
 public class A2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello new...............");
-
+           DisplayMessage dm=new DisplayMessage();
+           dm.displayInfo();
+           DisplayMessage dm1=new DisplayMessage("This is user defined constructor");
+           dm1.displayInfo();
 	}
 
 }
+
+
+
+

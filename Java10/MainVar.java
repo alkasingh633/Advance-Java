e49f19abeae9b5e972261d2a9ab47dde0fd3863b
Alka <alkasingh143179@gmail.com>
2026-03-22 09:15:22 +0530

    22-03-2026

---
+++ Java10/MainVar.java
@@ -0,0 +1,27 @@
+package Java10;
+
+
+
+public class MainVar {
+   //var i=10;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int a=10;
+           var b=20;
+           var value=10.34;
+           var c=0.0;
+           c=39.56;
+           String name=new String("This is string");
+           var name1 =new String("This is string with var");
+           int val[]=new int[] {23,45,67,89,12};
+           for(var v:val) {
+        	   System.out.println(v);
+           }
+           
+           
+	}
+
+}
+
+
+

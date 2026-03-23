90101232619a504dd6dc8dc25c86e6ae777aa242
Alka <alkasingh143179@gmail.com>
2026-03-23 12:07:27 +0530

    23-03-2026

--- Reduce/Ques1.java
+++ Reduce/Ques1.java
@@ -7,18 +7,20 @@ public class Ques1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	    List<Integer> numbers = Arrays.asList(10, 25, 5, 40, 30);
+	    List<Integer> list = Arrays.asList(10, 25, 5, 40, 30);
+	    int max = list.stream().reduce(Integer::max).get();
+	    System.out.println(max);
 
         // Using reduce to find maximum
-        Optional<Integer> max = numbers.stream()
-                                       .reduce((a, b) -> a > b ? a : b);
+//        Optional<Integer> max = numbers.stream()
+//                                       .reduce((a, b) -> a > b ? a : b);
 
         // Display result
-        if (max.isPresent()) {
-            System.out.println("Maximum number: " + max.get());
-        } else {
-            System.out.println("List is empty");
-        }
+//        if (max.isPresent()) {
+//            System.out.println("Maximum number: " + max.get());
+//        } else {
+//            System.out.println("List is empty");
+//        }
 	}
 
 }
---
+++ Reduce/Ques2.java
@@ -0,0 +1,10 @@
+package Reduce;
+
+public class Ques2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

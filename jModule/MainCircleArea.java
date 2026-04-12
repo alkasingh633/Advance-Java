b2cda7345363975aa04accc65e1737eec5f89a16
Alka <alkasingh143179@gmail.com>
2026-04-12 18:05:22 +0530

    12-04-2026

---
+++ jModule/MainCircleArea.java
@@ -0,0 +1,18 @@
+package jModule;
+
+
+//import edu.kiet.area.Circle;
+public class MainCircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//         Circle c =new Circle();
+//         c.getArea(23);
+        
+	}
+
+}
+
+
+

7785b34361b3613a31b9fbdb080a0f33cb643c7f
Alka <alkasingh143179@gmail.com>
2026-04-16 23:19:12 +0530

    16-04-2026

---
+++ Z_MyPractice/MainFunctionalInterface.java
@@ -0,0 +1,15 @@
+package Z_MyPractice;
+
+@FunctionalInterface
+interface Calc{
+	String square(int n1);
+}
+
+public class MainFunctionalInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

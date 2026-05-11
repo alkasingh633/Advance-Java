58d4eb7e46492e36422afa571bb763949a5da2ef
Alka <alkasingh143179@gmail.com>
2026-05-11 05:30:33 +0530

    11-05-2026

--- Map/MapEx.java
+++ Map/MapEx.java
@@ -8,7 +8,7 @@ public class MapEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> numbers = Arrays.asList(23,56, 78, 87, 45 ,32);
+		List<Integer> numbers = Arrays.asList(24, 56, 87 ,34 ,98);
 		System.out.println(numbers);
 		List<Integer> result = numbers.stream().filter(n->n<40).map(n-> n+8).toList();
 		System.out.println(result);
